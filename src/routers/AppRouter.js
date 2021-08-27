a6e43306ab2de56c26dcd83c46b148745c42a482
wildelmy <wildelmycolina@gmail.com>
2021-08-27 03:28:04 -0500

    crear rutas publicas y privadas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,1 +1,37 @@
-import { Link } from "react-router-dom";
+import React from 'react'
+import {
+  HashRouter as Router,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import AppMovie from '../components/formulario/AppMovie'
+import { AuthRouter } from './AuthRouter'
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+
+const [checking, setchecking] = useState(true)
+const [isLooggedIn, setIsLooggedIn] = useState(false)
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <PublicRoute
+          path="/auth"
+          component={AuthRouter}
+          isAuthenticated= {isLooggedIn}
+          />
+
+          <PrivateRoute
+          exact
+          path="/"
+          component={AppMovie}
+          isAuthenticated={isLooggedIn}
+          />
+    
+        </Switch>
+      </div>
+    </Router>
+  )
+}
